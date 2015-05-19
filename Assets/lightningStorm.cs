c7601d471687b5182c1fcfae150e334475a6bceb
Rasen <ramirezsa89@gmail.com>
2015-05-19 23:12:13 +0200

    LightningStorm added

---
+++ Assets/lightningStorm.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class lightningStorm : MonoBehaviour {
+
+	GameObject tree;
+	Vector3 pointa = Vector3.zero;
+	Vector3 pointb = Vector3.zero;
+	Vector3 pointc = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		GameObject directionalLight = GameObject.Find ("directionalLight");
+		GameObject playerLight = GameObject.Find ("playerLight");
+
+		directionalLight.GetComponent<Light> ().intensity = 0.1f;
+		playerLight.GetComponent<Light> ().enabled = true;
+		InvokeRepeating("launchLightning", 1, 1f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public class VertexConnection
+	{
+		public List<int> connections = new List<int>();
+	}
+	void launchLightning() {
+		Mesh mesh = (Mesh)GameObject.Find ("lightningTerrain").GetComponent<MeshFilter> ().mesh;
+		mesh.RecalculateBounds();
+		Vector3[] vertices = mesh.vertices;
+		GameObject lightning = Instantiate(Resources.Load("scenary/lightning"), Vector3.zero, Quaternion.identity) as GameObject;
+		Vector3 randomVertice = vertices[Random.Range (0, vertices.Length)];
+		lightning.transform.name = "lightning";
+		bool playerFuera = isPlayerOutside ();
+		if (isPlayerOutside() && Random.Range (1, 25) == 5) {
+			lightning.transform.position = GameObject.Find ("child").transform.TransformPoint (randomVertice);
+
+		} else {
+			lightning.transform.position = GameObject.Find ("lightningTerrain").transform.TransformPoint (randomVertice);
+		}
+
+
+		lightning.transform.SetParent (transform);
+		StartCoroutine (removeLightning(lightning));
+	}
+
+	IEnumerator removeLightning(GameObject lightning) {
+		yield return new WaitForSeconds (0.5f);
+		Destroy (lightning);
+	}
+
+	bool isPlayerOutside(){
+		GameObject player = GameObject.Find("child");
+		return player.GetComponent<childController> ().isOutside;
+	}
+}
---
+++ Assets/madLady.cs
@@ -0,0 +1,137 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class madLady : MonoBehaviour {
+
+	const int FOLLOW_CHILD = 1;
+	const int HIT_CHILD = 2;
+	const int PUSH_PLAYER = 3;
+	const int RUNAWAY = 4;
+	const int YELLING = 5;
+	const int STOP = 6;
+
+
+	GameObject childTarget;
+	public NavMeshAgent agent;
+	RaycastHit hit;
+	
+	public int state;
+	Vector3 startPoint;
+	public bool isRunning = false;
+
+	// Use this for initialization
+	void Start () {
+		childTarget = GameObject.Find ("child");
+		agent = GetComponent<NavMeshAgent> ();
+		state = FOLLOW_CHILD;
+		startPoint = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		switch (state) {
+		case FOLLOW_CHILD:
+			followChild();
+			break;
+		case YELLING:
+			int b1 = 2;
+			break;
+		case STOP:
+			if(!isRunning){
+				isRunning = true;
+				int seconds = Random.Range (1, 5);
+				StartCoroutine(stopFewSecondsAndReturnToFollowChild(seconds));
+			}
+
+			break;
+		case RUNAWAY:
+			if(!isRunning){
+				agent.SetDestination(startPoint);
+				isRunning = true;
+			}
+			if(transform.position.x == startPoint.x && transform.position.z == startPoint.z){
+				Destroy(gameObject);
+			}
+			break;
+		}
+	}
+
+	void followChild(){
+		agent.SetDestination (childTarget.transform.position);
+
+	}
+
+	void OnCollisionStay(Collision collision) {
+
+		switch (collision.transform.name) {
+		case "child":
+			if(state == FOLLOW_CHILD && !isRunning)
+			{
+				int[] randomChildCollisionActions = new int[] {YELLING, HIT_CHILD, HIT_CHILD, HIT_CHILD};
+				int actionAgainsChild = randomChildCollisionActions[Random.Range(0, randomChildCollisionActions.Length)];
+				switch(actionAgainsChild){
+					case YELLING:
+						stopMadLady();
+						yellingChild();
+					break;
+					case HIT_CHILD:
+						stopMadLady();
+						hitChild();
+						break;
+				}
+			}
+			break;
+		}
+		
+	}
+
+	void yellingChild(){
+		state = YELLING;
+		stopChild ();
+		int seconds = Random.Range (0, 3);
+		StartCoroutine (stopFewSecondsThenMeStopAndChildMoves(seconds));
+	}
+	void hitChild(){
+		state = HIT_CHILD;
+		stopChild();
+		int damageToChild = 2;
+		GameObject.Find("child").GetComponent<childController>().hitAndPain(damageToChild);
+		StartCoroutine (stopFewSecondsThenMeStopAndChildMoves(1));
+	}
+
+	void stopChild(){
+		int waitingChildState = 3;
+		GameObject.Find("child").GetComponent<childController>().agent.Stop ();
+		GameObject.Find("child").GetComponent<childController>().agent.ResetPath ();
+		GameObject.Find("child").GetComponent<childController>().state = waitingChildState;
+		GameObject.Find("child").GetComponent<childController>().isRandomState = false;
+		GameObject.Find("child").GetComponent<childController>().isRunning = true;
+	}
+	void startChild(){
+		int walkState = 0;
+		GameObject.Find("child").GetComponent<childController>().state = walkState;
+		GameObject.Find("child").GetComponent<childController>().isRunning = false;
+	}
+
+	IEnumerator stopFewSecondsThenMeStopAndChildMoves(int seconds){
+		isRunning = true;
+		yield return new WaitForSeconds(seconds);
+		isRunning = false;
+		state = STOP;
+		startChild ();
+	}
+
+	IEnumerator stopFewSecondsAndReturnToFollowChild(int seconds){
+		yield return new WaitForSeconds(seconds);
+		if(state != RUNAWAY)
+		{
+			state = FOLLOW_CHILD;
+			isRunning = false;
+		}
+	}
+
+	void stopMadLady(){
+		agent.ResetPath ();
+	}
+}
